68942073dc66ed17dda27292e4ebbaf7412a033a
quangnha330011002005 <trranquangnha@gmail.com>
2025-02-20 07:58:38 +0700

    pblap06

--- WebQLDaoTao/QLDiemThi.aspx.cs
+++ WebQLDaoTao/QLDiemThi.aspx.cs
@@ -25,7 +25,7 @@ namespace WebQLDaoTao
                 double diem= double.Parse(((TextBox)gvKetQua.Rows[i].FindControl("txtDiem")).Text);
                 kqDAO.Update(id,diem);
             }
-            Response.Write("<script> alert'(Lưu điểm thành công)' </script>");
+            Response.Write("<script> alert'(Lưu điểm thành côngg)' </script>");
         }
     }
 }
